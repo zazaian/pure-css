ef9242f007589f1320b91157edac52e77e78138c
Mike Zazaian <zazaian@gmail.com>
2017-04-19 19:44:46 -0400

    add property position tests

--- tests/Properties/PositionTests.elm
+++ tests/Properties/PositionTests.elm
@@ -6,8 +6,28 @@ import Properties.Position exposing (..)
 
 all : Test
 all =
-    describe "some stuff"
+    describe "position properties"
         [ test "absolute position" <|
             \() ->
                 position Absolute |> Expect.equal "position: absolute;"
+
+        , test "static position" <|
+            \() ->
+                position Static |> Expect.equal "position: static;"
+
+        , test "fixed position" <|
+            \() ->
+                position Fixed |> Expect.equal "position: fixed;"
+
+        , test "relative position" <|
+            \() ->
+                position Relative |> Expect.equal "position: relative;"
+
+        , test "initial position" <|
+            \() ->
+                position Initial |> Expect.equal "position: initial;"
+
+        , test "inherit position" <|
+            \() ->
+                position Inherit |> Expect.equal "position: inherit;"
         ]
