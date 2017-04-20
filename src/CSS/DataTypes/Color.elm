98ca84e72d1dabfbd79246ff8df2b14c39a51749
Mike Zazaian <zazaian@gmail.com>
2017-04-20 14:40:50 -0400

    add basic color keywords

---
+++ src/CSS/DataTypes/Color.elm
@@ -0,0 +1,25 @@
+module CSS.DataTypes.Color exposing (..)
+
+type Color =
+    BasicColorKeyword
+    | 
+
+
+type BasicColorKeword =
+     Black
+     | Silver
+     | Gray
+     | White
+     | Maroon
+     | Red
+     | Purple
+     | Fuchsia
+     | Green
+     | Lime
+     | Olive
+     | Yellow
+     | Navy
+     | Blue
+     | Teal
+     | Aqua
+
