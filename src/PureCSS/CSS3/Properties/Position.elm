645fb808c099be07eff5c628461211261632f671
Mike Zazaian <zazaian@gmail.com>
2017-04-21 19:02:39 -0400

    namespace Position and Relative properties

---
+++ src/PureCSS/CSS3/Properties/Position.elm
@@ -0,0 +1,15 @@
+module PureCSS.CSS3.Properties.Position exposing (..)
+
+import PureCSS.CSS3.DataTypes.Textual exposing (CSSKeywords)
+
+
+
+type PositionValue =
+  Static
+  | Absolute
+  | Fixed
+  | Relative
+  | CSSKeyword
+
+
+
---
+++ src/PureCSS/CSS3/Properties/Relative.elm
@@ -0,0 +1,11 @@
+module PureCSS.CSS3.Properties.Relative exposing (..)
+module PureCSS.CSS3.DataTypes.Textual exposing (CSSKeyword)
+
+
+
+type Relative =
+  Auto
+  | Length Int Size.Units
+  | CSSKeyword
+
+
