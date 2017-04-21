d8d4757e09eaed6877c7a7fe88bc3431a54620d2
Mike Zazaian <zazaian@gmail.com>
2017-04-21 14:40:26 -0400

    remove stale types from Numeric

--- src/CSS/DataTypes/Numeric.elm
+++ src/CSS/DataTypes/Numeric.elm
@@ -6,24 +6,3 @@ type Numeric =
 
 type Dimension =
     number Unit
-
-
-{-
-
-type Numeric =
-    Integer Int
-    | Number number
-    | Percentage
-
-
-type Percentage =
-    LengthPct number
-    | FrequencyPct number
-    | AnglePct number
-    | TimePct number
-    | NumberPct number
-
-Need to look up compatible and canonical units:
-4.5. Compatible Units
-
--}
