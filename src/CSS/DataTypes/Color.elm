4e134f26d13cd2597fa58fa6f6129a47114b26fe
Mike Zazaian <zazaian@gmail.com>
2017-04-21 14:54:04 -0400

    refine RGB/HSL types, add X11 Color keywords

--- src/CSS/DataTypes/Color.elm
+++ src/CSS/DataTypes/Color.elm
@@ -3,9 +3,24 @@ module CSS.DataTypes.Color exposing (..)
 import CSS.DataTypes.Numeric exposing (Numeric)
 
 type Color =
-    BasicColorKeyword
-    | RGB RGBTuple
-    | RGBA RGBATuple
+    ColorKeyword
+    | RGB TriColor
+    | RGBA QuadColor
+    | HSL TriColor
+    | HSLA QuadColor
+
+
+type alias TriColor =
+    ( Numeric, Numeric, Numeric )
+
+
+type alias QuadColor =
+    ( Numeric, Numeric, Numeric, Numeric )
+
+
+type ColorKeyword =
+     BasicColorKeyword
+     | X11ColorKeyword
 
 
 type BasicColorKeword =
@@ -27,17 +42,135 @@ type BasicColorKeword =
      | Aqua
 
 
-type RGBTuple =
-    RGB ( Numeric, Numeric, Numeric )
-
-
-type RGBATuple =
-    RGBA ( Numeric, Numeric, Numeric, Numeric )
-
-
-type HSLTuple =
-    HSL ( Numeric, Numeric, Numeric )
-
-
-type HSLATuple =
-    HSLA ( Numeric, Numeric, Numeric, Numeric )
+type X11ColorKeyword =
+    AliceBlue
+    | AntiqueWhite
+    | Aquamarine
+    | Azure
+    | Beige
+    | Bisque
+    | Black
+    | BlanchedAlmond
+    | BlueViolet
+    | Brown
+    | Burlywood
+    | CadetBlue
+    | Chartreuse
+    | Chocolate
+    | Coral
+    | Cornflower
+    | Cornsilk
+    | Crimson
+    | Cyan
+    | DarkBlue
+    | DarkCyan
+    | DarkGoldenrod
+    | DarkGray
+    | DarkGreen
+    | DarkKhaki
+    | DarkMagenta
+    | DarkOliveGreen
+    | DarkOrange
+    | DarkOrchid
+    | DarkRed
+    | DarkSalmon
+    | DarkSeaGreen
+    | DarkSlateBlue
+    | DarkSlateGray
+    | DarkTurquoise
+    | DarkViolet
+    | DeepPink
+    | DeepSkyBlue
+    | DimGray
+    | DodgerBlue
+    | Firebrick
+    | FloralWhite
+    | ForestGreen
+    | Gainsboro
+    | GhostWhite
+    | Gold
+    | Goldenrod
+    | WebGray
+    | WebGreen
+    | GreenYellow
+    | Honeydew
+    | HotPink
+    | IndianRed
+    | Indigo
+    | Ivory
+    | Khaki
+    | Lavender
+    | LavenderBlush
+    | LawnGreen
+    | LemonChiffon
+    | LightBlue
+    | LightCoral
+    | LightCyan
+    | LightGoldenrod
+    | LightGray
+    | LightGreen
+    | LightPink
+    | LightSalmon
+    | LightSeaGreen
+    | LightSkyBlue
+    | LightSlateGray
+    | LightSteelBlue
+    | LightYellow
+    | LimeGreen
+    | Linen
+    | Magenta
+    | WebMaroon
+    | MediumAquamarine
+    | MediumBlue
+    | MediumOrchid
+    | MediumPurple
+    | MediumSeaGreen
+    | MediumSlateBlue
+    | MediumSpringGreen
+    | MediumTurquoise
+    | MediumVioletRed
+    | MidnightBlue
+    | MintCream
+    | MistyRose
+    | Moccasin
+    | NavajoWhite
+    | NavyBlue
+    | OldLace
+    | OliveDrab
+    | Orange
+    | OrangeRed
+    | Orchid
+    | PaleGoldenrod
+    | PaleGreen
+    | PaleTurquoise
+    | PaleVioletRed
+    | PapayaWhip
+    | PeachPuff
+    | Peru
+    | Pink
+    | Plum
+    | PowderBlue
+    | WebPurple
+    | RebeccaPurple
+    | RosyBrown
+    | RoyalBlue
+    | SaddleBrown
+    | Salmon
+    | SandyBrown
+    | SeaGreen
+    | Seashell
+    | Sienna
+    | SkyBlue
+    | SlateBlue
+    | SlateGray
+    | Snow
+    | SpringGreen
+    | SteelBlue
+    | Tan
+    | Thistle
+    | Tomato
+    | Turquoise
+    | Violet
+    | Wheat
+    | WhiteSmoke
+    | YellowGreen
