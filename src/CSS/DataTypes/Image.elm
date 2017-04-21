e78aa9e5cfe6c60b266aa8fc7e18468b53a1f65a
Mike Zazaian <zazaian@gmail.com>
2017-04-21 15:14:34 -0400

    add placeholders for Image and Position types

---
+++ src/CSS/DataTypes/Image.elm
@@ -0,0 +1,7 @@
+module CSS.DataTypes.Image exposing (..)
+
+{-
+Defined by the spec at:
+https://www.w3.org/TR/css3-images/
+-}
+
---
+++ src/CSS/DataTypes/Position.elm
@@ -0,0 +1,9 @@
+module CSS.DataTypes.Position exposing (..)
+
+{-
+
+Defined in the spec at:
+https://drafts.csswg.org/css-backgrounds-3/
+
+-}
+
