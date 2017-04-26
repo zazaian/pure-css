179be0447a9ac504ff736579855f87459d99b25b
Mike Zazaian <zazaian@gmail.com>
2017-04-25 20:12:12 -0400

    use Distance.toString for Width

--- src/PureCSS/Properties/Width.elm
+++ src/PureCSS/Properties/Width.elm
@@ -1,13 +1,7 @@
 module PureCSS.Properties.Width exposing (..)
 
-import String exposing (toLower)
-import PureCSS.DataTypes.Distance exposing
-    ( Distance(..)
-    , LengthUnit(..)
-    )
-import PureCSS.DataTypes.Textual exposing
-    (CSSKeyword(..)
-    )
+import PureCSS.DataTypes.Distance as Distance
+import PureCSS.DataTypes.Distance exposing (Distance)
 import PureCSS.Property exposing
     (PropertyName(Width)
     , Property
@@ -17,18 +11,4 @@ import PureCSS.Property exposing
 
 width : Distance -> Property
 width distance =
-    let
-        value = case distance of
-            Length number unit ->
-                (toString number) ++ (unit |> toString |> toLower)
-
-            Percent number ->
-                (toString number) ++ "%"
-
-            Keyword keyword ->
-                keyword
-                |> toString
-                |> toLower
-
-    in
-        prop Width value
+    prop Width (Distance.toString distance)
