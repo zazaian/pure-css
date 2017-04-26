df4f3addbcc2aaaa835b5c5e9b2503b3781189bb
Mike Zazaian <zazaian@gmail.com>
2017-04-25 20:21:29 -0400

    add all box offset tests

--- tests/Tests/Properties/BoxOffsetTests.elm
+++ tests/Tests/Properties/BoxOffsetTests.elm
@@ -9,9 +9,7 @@ import PureCSS.DataTypes.Distance exposing
     )
 import PureCSS.DataTypes.Textual exposing (CSSKeyword(..))
 import PureCSS.Property exposing
-    (PropertyName(Top)
-    , Property
-    , prop
+    (PropertyName(Top,Right,Bottom,Left)
     )
 
 
@@ -31,5 +29,40 @@ all =
             \() ->
                 top (Keyword Inherit) |> Expect.equal { name = Top, val = "inherit" }
 
+        , test "right as length" <|
+            \() ->
+                right (Length 100 Px) |> Expect.equal { name = Right, val = "100px" }
+
+        , test "right as percentage" <|
+            \() ->
+                right (Percent 50) |> Expect.equal { name = Right, val = "50%" }
+
+        , test "right as CSS keyword" <|
+            \() ->
+                right (Keyword Inherit) |> Expect.equal { name = Right, val = "inherit" }
+
+        , test "bottom as length" <|
+            \() ->
+                bottom (Length 100 Px) |> Expect.equal { name = Bottom, val = "100px" }
+
+        , test "bottom as percentage" <|
+            \() ->
+                bottom (Percent 50) |> Expect.equal { name = Bottom, val = "50%" }
+
+        , test "bottom as CSS keyword" <|
+            \() ->
+                bottom (Keyword Inherit) |> Expect.equal { name = Bottom, val = "inherit" }
+
+        , test "left as length" <|
+            \() ->
+                left (Length 100 Px) |> Expect.equal { name = Left, val = "100px" }
+
+        , test "left as percentage" <|
+            \() ->
+                left (Percent 50) |> Expect.equal { name = Left, val = "50%" }
+
+        , test "left as CSS keyword" <|
+            \() ->
+                left (Keyword Inherit) |> Expect.equal { name = Left, val = "inherit" }
         ]
 
