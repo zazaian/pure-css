6ba3d783973b4561d8ea1dc84dab70b69d8bee64
Mike Zazaian <zazaian@gmail.com>
2017-04-25 20:55:29 -0400

    don't expose String.toLower in BoxOffset

--- src/PureCSS/Properties/BoxOffset.elm
+++ src/PureCSS/Properties/BoxOffset.elm
@@ -1,6 +1,5 @@
 module PureCSS.Properties.BoxOffset exposing (..)
 
-import String exposing (toLower)
 import PureCSS.DataTypes.Distance as Distance
 import PureCSS.DataTypes.Distance exposing (Distance)
 import PureCSS.Property exposing
